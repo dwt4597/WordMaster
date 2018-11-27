755b46ffed30ea3d6d3dfc9d9e08f8fc6b346a92
ijs9200 <ijs9200@naver.com>
2018-11-27 21:54:49 +0900

    first

---
+++ src/com/biz/word/WordBook.java
@@ -0,0 +1,25 @@
+package com.biz.word;
+
+public class WordBook {
+	public static String[] word = {
+			"mistletoe:겨우살이의 일종, 꽃말은 「정복」, 겨우살이",
+			"nebulous:흐린, 막연한, 성운의",
+			"beholden:신세를 지고, 은혜를 입고, 신세진",
+			"deafness:귀가 들리지 않는 것, 귀를 기울이지 않는 것",
+			"occupancy:점유, 점거, 점유 기간",
+			"winsome:매력 있는, 쾌활한, 사람을 끄는",
+			"augmentation:증가, 증대, 증가물",
+			"shingles:대상 포진",
+			"sonny:아가, 얘야, 풋내기",
+			"thankless:감사할 줄 모르는, 고마워할 것 같지 않은, 보람 없는",
+			"clink:교도소, 유치장, 쨍 울리다",
+			"evasively:회피하여, 얼버무리며",
+			"fief:봉토, 세습 영지, 영지",
+			"hygiene:위생학, 위생, 건강법",
+			"presentable:남 앞에 내놓을 만한, 상연할 수 있는, 보기 흉하지 않은",
+			"jib:지브, 주저하다, 한쪽 뱃전에서 다른 쪽 뱃전으로 돌리다",
+			"scarecrow:허수아비, 허깨비, 초라한 사람",
+			"bastion:요새, 능보, 성채",
+			"fungus:곰팡이, 균, 버섯" 
+			};
+}
